7481dc220ad780dcddd2d80809e7c474ac0c0881
Sohrab <s.saboori@rikaweb.com>
2022-10-03 09:44:44 -0400

    responsive bug fixed

--- src/components/PaginateResult.tsx
+++ src/components/PaginateResult.tsx
@@ -24,16 +24,9 @@ const PaginateResult = (props: {
 
   return (
     <>
-      <div className="flex items-center justify-between border-t border-gray-200 bg-gray-100 px-4 py-5 sm:px-6">
-        <div className="flex flex-1 justify-between sm:hidden">
-          <button className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">
-            Previous 
-          </button>
-          <button className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">
-            Next
-          </button>
-        </div>
-        <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+      <div className="flex items-center justify-between px-4 py-5 bg-gray-100 border-t border-gray-200 sm:px-6">
+       
+        <div className=" sm:flex sm:flex-1 sm:items-center sm:justify-between">
           <div>
             <p className="text-sm text-gray-700">
               Showing{" "}
